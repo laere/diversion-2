4c1afb3d0564385f87bb31ae9795972725c5a16b
Zack Bostian <bostianz@gmail.com>
2016-04-03 15:51:25 -0400

    fixed home page on mobile, also might swap back to hashhistory

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Heading from '../components/Heading';
+import MainContent from '../components/MainContent';
 
 export default class HomeContainer extends Component {
   render() {
     return (
-      <div className="homeContainer">
-        <Heading style="homeHeader header" header="Home" />
+      <MainContent>
+        <Heading style="homeHeader header" header="Home"/>
         <div className="homeContent">
           <h2>Welcome to Diversion!</h2>
 
@@ -16,9 +17,9 @@ export default class HomeContainer extends Component {
           <p>Ironically I built this while always keeping one of my favorite twitch streamers on in the background, and although I haven't been as distracted by Twitch presently.
           I thought it would be a cool project, to really focus, and try to create something that keeps pushing my skills to the next level.</p>
 
-        <a href="http://github.com/laere/diversion-2">Click here</a><span>to see the projects source code.</span> <a href="https://twitter.com/zack_bostian">And here</a><span>to follow me on twitter!</span>
+          <a href="http://github.com/laere/diversion-2">Click here</a><span>to see the projects source code.</span> <a href="https://twitter.com/zack_bostian">And here</a><span>to follow me on twitter!</span>
         </div>
-      </div>
+      </MainContent>
     );
   }
 }
